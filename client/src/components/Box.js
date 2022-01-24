6a5ee82d5906d44a97910b231fa64daa1a77f371
Tanay <tanaydwivedi456@gmail.com>
2022-01-24 20:48:31 +0530

    feat:written winniing and game reloading logic

--- client/src/components/Box.js
+++ client/src/components/Box.js
@@ -7,50 +7,128 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
+const itemArray=new Array(9).fill("empty");
 
 
 
 const Box=()=>{
+  const [isCircle, setIsCircle] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCircle(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  }
+
+  const isWinner=()=>{
+    if(itemArray[0]===itemArray[1]&&
+      itemArray[0]===itemArray[2]&&
+      itemArray[0]!=="empty"){
+        setWinMessage(`${itemArray[0]} wins`)
+
+    }
+    else if( itemArray[3]===itemArray[4]&&
+      itemArray[3]===itemArray[5]&&
+      itemArray[3]!=="empty"){
+        setWinMessage(`${itemArray[3]} wins`)
+    }
+    else if( itemArray[6]===itemArray[7]&&
+    itemArray[6]===itemArray[8]&&
+    itemArray[6]!=="empty"){
+      setWinMessage(`${itemArray[6]} wins`)
+    }
+
+    else if( itemArray[0]===itemArray[3]&&
+  itemArray[3]===itemArray[6]&&
+  itemArray[0]!=="empty"){
+    setWinMessage(`${itemArray[0]} wins`)
+  }
+
+  else if( itemArray[1]===itemArray[4]&&
+  itemArray[1]===itemArray[6]&&
+  itemArray[1]!=="empty"){
+    setWinMessage(`${itemArray[1]} wins`)
+  }
+  else if( itemArray[2]===itemArray[5]&&
+  itemArray[2]===itemArray[8]&&
+  itemArray[2]!=="empty"){
+
+    setWinMessage(`${itemArray[2]} wins`)
+  }
+
+  else if( itemArray[0]===itemArray[4]&&
+  itemArray[0]===itemArray[8]&&
+  itemArray[0]!=="empty"){
+    setWinMessage(`${itemArray[0]} wins`)
+  }
+
+  else if( itemArray[2]===itemArray[4]&&
+  itemArray[2]===itemArray[6]&&
+  itemArray[2]!=="empty"){
+    setWinMessage(`${itemArray[2]} wins`)
+  }}
+
+
+
+
+
+
   return(
 
-
+<div className="container-fluid">
   
-
-
-<div class="container">
-  
-  <div class="row">
-    <div class="col">
-      1 of 3
+  <div className="row">
+    <div className="col">
+      <div className="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
-    <div class="col">
-      2 of 3
+    <div className ="col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
-    <div class="col">
-      3 of 3
+    <div className ="col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
   </div>
   
-  <div class="row">
-    <div class="col">
-      1 of 3
+  <div className ="row">
+    <div className ="col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
+      
     </div>
-    <div class="col">
-      2 of 3
+    <div className ="col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
-    <div class="col">
-      3 of 3
+    <div className ="col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col">
-      1 of 3
+  <div className ="row">
+    <div className = "col">
+      <div className ="card bg-info text-black">
+        <div className ="card-body"></div>
+      </div>
     </div>
-    <div class="col">
-      2 of 3
+    <div className ="col">
+      <div className ="card bg-info text-white">
+        <div className ="card-body"></div>
+      </div>
     </div>
-    <div class="col">
-      3 of 3
+    <div className ="col">
+      <div className ="card bg-info text-white">
+        <div className ="card-body"></div>
+      </div>
     </div>
   </div>
 
